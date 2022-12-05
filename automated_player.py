97b846b2d1f1078db40436c904c8877870f86770
ChuyangL <chuyangli1998@outlook.com>
2022-12-04 22:21:07 -0600

    Update 2 files

--- automated_player.py
+++ automated_player.py
@@ -1,11 +1,12 @@
 import numpy as np
+import math
 from typing import List
 
 
 class Board:
     def __init__(self):
         self.board = np.array([['.'] * 5] * 5)
-        self.tops = np.array([0] * 5)
+        self.tops = np.array([4] * 5)
         self.player_1 = np.array([[0] * 5] * 5)
         self.player_2 = np.array([[0] * 5] * 5)
         self.encode_board = []
@@ -23,7 +24,7 @@ class Board:
         print('bit')
         for row in self.bit_columns:
             for column in row:
-                print(f'{bin(column)[2:]:0>6}', end='  ')
+                print(f'{bin(column)[2:]:0>5}', end='  ')
             print('\n', end='')
         print('\n', end='')
 
@@ -33,7 +34,7 @@ class Board:
         for row in self.bit_columns:
             tmp = []
             for column in row:
-                tmp.append(eval(f'0b{bin(column)[2:]:0>6}'))
+                tmp.append(eval(f'0b{bin(column)[2:]:0>5}'))
             result.append(tmp)
         return result
 
@@ -46,23 +47,27 @@ class Board:
             b = self.player_2
         elif target == 'encoding':
             b = self.encode_board
+        elif target == 'top':
+            print(target)
+            print(self.tops)
+            return
         elif target == 'bit':
             self.print_bit_columns()
             return
 
+        print(target)
         for row in b:
-            print(target)
             print(row)
-            print('\n')
+        print('\n')
 
     def print_all(self):
-        targets = ['board', 'player_1', 'player_2', 'encoding', 'bit']
+        targets = ['board', 'player_1', 'player_2', 'encoding', 'bit', 'top']
 
         for target in targets:
             self.print_board(target)
 
     def change_bit_columns(self, player: int, row: int, column):
-        self.bit_columns[player - 1][5 - column] ^= 1 << row
+        self.bit_columns[player - 1][4 - column] ^= 1 << (4 - row)
         self.print_board('bit')
 
     def find_position(self, column: int):
@@ -71,11 +76,12 @@ class Board:
         else:
             raise ValueError('Column Full. Invalid move!')
 
-    def is_winning(self, player: int, board=None) -> bool:
+    def is_winning(self, player: int, board: np.array = None) -> bool:
         # TODO: Maggie
         """
         Find out if the input player has connect 3 pieces on board.
 
+        :param board:
         :param player: 1 for first player and 2 for second player.
 
         :return: if this player has connect 3.
@@ -126,11 +132,6 @@ class Board:
 
         return self.is_winning(3 - player, rival_board)
 
-    def calculate_freedom(self, player: int, position: List[int]) -> int:
-        freedom = 0
-        player_max_height = self.bit_columns[player - 1]
-
-
     def find_optimized_solution(self, player: int) -> List[int]:
         # TODO: Chuyang
         """
@@ -141,19 +142,29 @@ class Board:
         :return: place to make move [row, column].
         """
         # If the board is empty, put the first piece in the middle
-        if self.tops == np.array([0] * 5):
+        if list(self.tops) == [4, 4, 4, 4, 4]:
             return [4, 2]
         else:
             # Iterate all 5 (or less than 5) positions to find the best place to put the piece
-            result = None
-            result_freedom = 0
+            result = [self.tops[2], 2]
+            max_height = np.max(self.bit_columns)
+            try:
+                max_position = list(self.bit_columns[0]).index(max_height)
+            except ValueError:
+                max_position = list(self.bit_columns[1]).index(max_height)
+            # print(max_height)
+            # print(max_position)
+            dist = 5
             opponent_winning = False
-            for position in self.tops:
+            for i in range(len(self.tops)):
                 # If the column is full, skip to the next column
-                if position[0] < 0:
+                position = self.tops[i]
+                if position < 0:
                     continue
                 else:
-                    r, c = position[0], position[1]
+                    r, c = position, i
+                    # print(r)
+                    # print(c)
                     # A temporary board to see the result
                     new_board_1 = self.player_1.copy()
                     new_board_2 = self.player_2.copy()
@@ -163,15 +174,16 @@ class Board:
                     player_2_winning = self.is_winning(player, new_board_2)
                     # If this move cause immediate winning, return this position
                     if (player_1_winning and player == 1) or (player_2_winning and player == 2):
-                        return position
+                        return [r, c]
                     # if this position does not cause immediate winning,
                     # but blocks opponent from winning
                     # record the position and iterate next position to see if there are better solution
                     elif (player_1_winning and player == 2) or (player_2_winning and player == 1):
-                        result = position
+                        result = [r, c]
                         opponent_winning = True
                     # If none of these happen
                     else:
+                        print([r, c])
                         if not opponent_winning:
                             tmp_top = [r - 1, c]
                             if tmp_top[0] < 0:
@@ -180,11 +192,21 @@ class Board:
                                 # See if this move will cause opponent winning in the next round
                                 # If not, find the position with maximum freedom (chance of winning)
                                 if not self.cause_opponent_winning(player, tmp_top):
-                                    freedom = self.calculate_freedom(player, position)
-                                    if not result or freedom > result_freedom:
-                                        result = position
-                                        result_freedom = freedom
-
+                                    height = self.bit_columns[player - 1][c]
+                                    if height != 0:
+                                        height = int(math.log(height, 2) + 1)
+                                        print(height)
+                                    if r != 4 - height:
+                                        continue
+                                    else:
+                                        if c == max_position:
+                                            result = [r, c]
+                                            dist = 0
+                                        elif height < max_height:
+                                            if not result or abs(c - max_position) < dist:
+                                                result = [r, c]
+                                                dist = abs(c - max_position)
+        # print(result)
         return result
 
     def make_move(self, player: int, row: int, column: int):
@@ -212,20 +234,36 @@ class Board:
 
         # 3. update bit_columns
         # for each player's bit_column, each element represents the bit value for the entire column
-
-        if player == 1:
-            self.bit_columns[0] = [''.join(str(row[i]) for row in self.player_1) for i in range(5)]
-        else:
-            self.bit_columns[1] = [''.join(str(row[i]) for row in self.player_2) for i in range(5)]
+        self.change_bit_columns(player, row, column)
+        # print(self.bit_columns)
+        self.tops[column] = row - 1
+        self.print_board('board')
 
     def play_game(self):
-        # TODO: Chuyang
-        pass
+        """
+        Contributor: Chuyang Li
+
+        :return: (None)
+        """
+        player_1 = input('For player 1, please choose from automated/manual: ')
+        player_2 = input('For player 2, please choose from automated/manual: ')
+
+        round = 0
+        while True:
+            round += 1
+            player = 2 - (round % 2)
+            if eval(f'player_{player}').lower() == 'automated':
+                row, column = self.find_optimized_solution(player)
+            elif eval(f'player_{player}').lower() == 'manual':
+                column = int(input('Please make your move (input column only, starts from 0): '))
+                row = self.tops[column]
+            self.make_move(player, row, column)
+            if self.is_winning(player):
+                print(f'Player {player} won the game.')
+                break
 
 
 if __name__ == "__main__":
     a = Board()
-    a.change_bit_columns(1, 3, 2)
-    a.change_bit_columns(0, 2, 2)
-    print(a.get_bit_value())
+    a.play_game()
 
